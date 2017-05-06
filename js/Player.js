7a02b156656564eb049e927f4222b763207e6a04
Duncan Bauer <dbauer@ucsc.edu>
2017-05-06 14:30:21 -0700

    fixed player stopping when switching direction

--- js/Player.js
+++ js/Player.js
@@ -33,10 +33,10 @@ function Player(game, atlas_key, atlas_frame, x, y) {
 	
 	// Key Bindings
 	this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onDown.add(this.moveRight, this);
-	this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onUp.add(this.stop, this);
+	this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onUp.add(this.stopMovement, this);
 	
 	this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT).onDown.add(this.moveLeft, this);
-	this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT).onUp.add(this.stop, this);
+	this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT).onUp.add(this.stopMovement, this);
 	
 	this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(this.jump, this);
 	this.game.input.keyboard.addKey(Phaser.Keyboard.D).onDown.add(this.dash, this);
@@ -45,51 +45,75 @@ function Player(game, atlas_key, atlas_frame, x, y) {
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.update = function() {	
 
-		
-	if(this.body.touchingRight || this.body.touchingLeft) {
-		this.dashCancelHorizontal();
-	}
+	// If the player makes contact with an obstacle, dashing will stop
+	//if(this.body.touchingRight || this.body.touchingLeft || this.body.touchingUp || this.body.touchingDown) {
+	//	this.dashCancel();
+	//}
 	
-	if(this.body.touchingUp || this.body.touchingDown) {
-		this.dashCancelVertical();
-	}
+	/*
+	 * START HORIZONTAL DASHING
+	 */
 
+	// Dashes player to the right until the dashDistance is covered
 	if(this.dashingRight && this.oldPosX - this.position.x > this.dashDistanceX) {
 		this.body.velocity.x = 1000;
 	}
+	// End the dash after the distance has been covered
 	else if(this.dashingRight) {
 		this.dashingRight = false;
 		this.justDashed = true;
 	}
 	
+	// Dashes player to the left until the dashDistance is covered
 	if(this.dashingLeft && this.oldPosX - this.position.x < this.dashDistanceX) {
 		this.body.velocity.x = -1000;
 	}
+	// End the dash after the distance has been covered
 	else if(this.dashingLeft) {
 		this.dashingLeft = false;
 		this.justDashed = true;
 	}
 
+	/*
+	 * END HORIZONTAL DASHING
+	 * START VERTICAL DASHING
+	 */
+
+		/*
+		 * Vertical dashing doesnt work, distance is never met. I think this.oldPosY - this.position.y is incorrect
+		 */
+		 
+	 // Debugging 
+	console.log(this.oldPosY);
+	console.log(this.position.y);
+	console.log(this.oldPosY - this.position.y);
+	console.log(this.dashDistanceY);
+	
+	// Dashes player down until the dashDistance is covered
 	if(this.dashingDown && this.oldPosY - this.position.y < this.dashDistanceY) {
 		this.body.velocity.y = 1000;
 	}
+	// End the dash after the distance has been covered
 	else if(this.dashingDown) {
 		this.dashingDown = false;
 		this.justDashed = true;
 	}
 	
-	console.log(this.oldPosY);
-	console.log(this.position.y);
-	console.log(this.oldPosY - this.position.y);
-	console.log(this.dashDistanceY);
+	// Dashes player up until the dashDistance is covered
 	if(this.dashingUp && this.oldPosY - this.position.y > this.dashDistanceY) {
 		this.body.velocity.y = -1000;
 	}
+	// End the dash after the distance has been covered
 	else if(this.dashingUp) {
 		this.dashingUp = false;
 		this.justDashed = true;
 	}
 	
+	/*
+	 * END HORIZONTAL DASHING
+	 */
+	
+	// Ends dash
 	if(this.justDashed) {
 		this.body.velocity.x = 0;
 		this.body.velocity.y = 0;
@@ -98,6 +122,8 @@ Player.prototype.update = function() {
 }
 
 Player.prototype.moveRight = function() {
+	// Lets player move right when they're grounded
+	// Must be editted for movement in air
 	if(this.body.touching.down) {		
 		//  Move to the right
 		this.facingForward = true;
@@ -107,16 +133,24 @@ Player.prototype.moveRight = function() {
 }
 
 Player.prototype.moveLeft = function() {
+	// Lets player move left when they're grounded
+	// Must be editted for movement in air
 	if(this.body.touching.down) {		
-		//  Move to the right
+		//  Move to the left
 		this.facingForward = false;
 		this.scale.x = 1/2;
 		this.body.velocity.x = -300;
 	}
 }
 
-Player.prototype.stop = function() {
-	if(!this.jumping) {
+/*
+ * IS CALLED WHEN THE LEFT AND RIGHT ARROW KEYS ARE RELEASED
+ */
+Player.prototype.stopMovement = function() {
+	var cursors = this.game.input.keyboard.createCursorKeys();	
+	
+	// Stops character movement when not jumping and not moving in another direction
+	if(!this.jumping && !(cursors.left.isDown || cursors.right.isDown)) {
 		this.body.velocity.x = 0;
 		this.body.velocity.y = 0;
 	}
@@ -159,11 +193,18 @@ Player.prototype.dash = function() {
 	}
 }
 
+/*
+ * CHECKS IF PLAYER IS DASHING OR NOT
+ */
 Player.prototype.isDashing = function() {
 	return (this.dashingRight || this.dashingLeft || this.dashingUp || this.dashingDown);
 }
 
+/*
+ * IS CALLED WHEN THE SPACEBAR IS PRESSED - JUMPS CHARACTER IF NOT JUMPING
+ */
 Player.prototype.jump = function() {
+	// Executes if the character is not jumping
 	if(!this.jumping) {
 		this.jumping = true;
 		// Play jump sound
@@ -173,6 +214,9 @@ Player.prototype.jump = function() {
 	}
 }
 
+/*
+ * IS CALLED ON COLLISION WITH THE FLOOR - ENDS JUMP IF NOT JUMPING
+ */
 Player.prototype.touchDown = function() {
 	if(this.jumping) {
 		this.jumping = false;
@@ -180,12 +224,13 @@ Player.prototype.touchDown = function() {
 	}
 }
 
-Player.prototype.dashCancelHorizontal = function() {
+/*
+ * IS CALLED ON COLLISION WITH OBJECT CANCELS DASHING
+ */
+Player.prototype.dashCancel = function() {
 	this.dashingRight = false;
 	this.dashingLeft = false;
-}
-
-Player.prototype.dashCancelVertical = function() {
 	this.dashingUp = false;
 	this.dashingDown = false;
+	this.justDashed = true;
 }
