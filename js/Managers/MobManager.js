9a99561e79122965d719badcb08e96760bb4f022
Duncan <duncanebauer@gmail.com>
2017-05-09 16:14:40 -0700

    Added mob manager and first mob prefab

---
+++ js/Managers/MobManager.js
@@ -0,0 +1,17 @@
+var MobManager = function(game) {
+	this.enemies = game.add.group();
+	this.enemies.enableBody = true;
+	this.game = game;
+};
+	
+MobManager.prototype = {
+	spawnEnemies: function(){
+		this.enemies.add(new Mob(this.game, 
+					    'character_atlas', 
+					    'WalkLeft_MouthOpen_Red3', 
+					    this.game.width/2 + 100, 
+					    this.game.height/2));
+		
+	}
+}
+
--- js/main.js
+++ js/main.js
@@ -19,20 +19,26 @@ Game.prototype = {
 		this.bkgd = this.add.tileSprite(0, 0, this.game.width, this.game.height, 'background00');
 		this.bkgd.height = 600;
 		this.bkgd.width = 1000;	
-		
-		//this.stage = new Stage(this.game);
 
-		this.game.world.setBounds(0,0,2400,1800);
+		// Set world bounds
+		this.game.world.setBounds(0, 0, 2400,1800);
 
+		// Create world
 		this.world = new World(this.game);
 		this.world.loadFloor();
 		
 		this.player = new Player(this.game, 'character_atlas', 'WalkLeft_MouthOpen_Purple3', this.game.width/2, this.game.height/2);
 		this.game.add.existing(this.player);
 
-		//this.camera = new Phaser.Camera(this.game, 0, 0, 0, 1000, 600);
+		// Create camera and lock it to the player with mario-esque deadzone
 		this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_TOPDOWN_TIGHT, 0.75, 0.75);		
 		this.game.camera.deadzone = new Phaser.Rectangle(100, 100, 400, 400);
+
+		this.MobManager = new MobManager(this.game);
+		this.MobManager.spawnEnemies();
+		// Test mob
+		//this.mob = new Mob(this.game, 'character_atlas', 'WalkLeft_MouthOpen_Red3', this.game.width/2 + 100, this.game.height/2);
+		//this.game.add.existing(this.mob);
 	},
 	
 	update:function() {		
@@ -43,9 +49,11 @@ Game.prototype = {
 				this.player.dashCancel();
 			}
 		}
+
+		this.game.physics.arcade.collide(this.MobManager.enemies.children, this.world.floor.children);
 	},
 	
-	// End the this.game and return to the main menu
+	// End the game and return to the main menu
 	endGame: function(end) {
 		console.log('Game: endGame');
 		this.speed = 5;
