bf21159be0628ba4b00546bf29c2a0bce40638d4
Duncan <duncanebauer@gmail.com>
2017-05-23 11:08:40 -0700

    fixed platform json

--- js/Prefabs/PlatformA.js
+++ js/Prefabs/PlatformA.js
@@ -9,7 +9,7 @@ PlatformA.prototype.constructor = PlatformA;
 
 PlatformA.prototype.createPlat = function() {
 	for(i=0; i<1000/64; i+=1) {
-		let temp = this.add(new Tile(this.game, 'tile_atlas', 'WaveCandy_Square', 32 + (i * 32), 568, 0.5, 0.5));
+		let temp = this.add(new Tile(this.game, 'platform_atlas', 'platform0', 32 + (i * 32), 568, 0.5, 0.5));
 	}
 }
 
