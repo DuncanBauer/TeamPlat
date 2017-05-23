d64dd8542b03bdcfe60c03b4895682a8b574bd24
Duncan <duncanebauer@gmail.com>
2017-05-23 13:30:38 -0700

    building world

--- js/WorldMap.js
+++ js/WorldMap.js
@@ -17,19 +17,43 @@ World.prototype = Object.create(Phaser.Group.prototype);
 World.prototype.constructor = World;
 
 World.prototype.loadFloor = function(atlas, frame) {
-	let temp = this.ground.add(new PlatformA(this.game, atlas, frame, 15));
-	temp.x = 500;
-	temp.y = 500;
+	let temp = this.ground.add(new PlatformA(this.game, atlas, frame, 32));
+	temp.x = -32;
+	temp.y = 1000;
+	
+	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 6));
+	temp.x = 981 + 224;
+	temp.y = 900;
+
+	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 4));
+	temp.x = 1173 + 192;
+	temp.y = 804;
+
+	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 7));
+	temp.x = 688 + 224;
+	temp.y = 718;
+
+	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 5));
+	temp.x = 664;
+	temp.y = 604;
 }
 
 World.prototype.loadWalls = function(atlas, frame) {
-	let temp = this.walls.add(new WallA(this.game, atlas, frame, 15));
-	temp.x = 500;
-	temp.y = 500;
+	let temp = this.walls.add(new WallA(this.game, atlas, frame, 3));
+	temp.x = 1141 + 224;
+	temp.y = 804;
+
+	temp = this.walls.add(new WallA(this.game, atlas, frame, 5));
+	temp.x = 588 + 224;
+	temp.y = 604;
+
+	temp = this.walls.add(new WallA(this.game, atlas, frame, 5));
+	temp.x = 588 + 64 + 12;
+	temp.y = 604 - (5*32);
 }
 
 World.prototype.loadEnemies = function() {
-	this.enemies.add(new Mob(this.game, 'character_atlas', 'WalkLeft_MouthOpen_Red3', this.game.width/2 + 100, this.game.height/2, this));
+	//this.enemies.add(new Mob(this.game, 'character_atlas', 'WalkLeft_MouthOpen_Red3', this.game.width/2 + 100, this.game.height/2, this));
 }
 
 World.prototype.loadObstacles = function() {
