d3c23fafedd6743a31daa116b387f1610aaefc60
exp13 <switkin@ucsc.edu>
2017-05-23 14:58:55 -0700

    more platform placements and minor tweaks

--- js/WorldMap.js
+++ js/WorldMap.js
@@ -19,37 +19,57 @@ World.prototype.constructor = World;
 World.prototype.loadFloor = function(atlas, frame) {
 	let temp = this.ground.add(new PlatformA(this.game, atlas, frame, 32));
 	temp.x = -32;
-	temp.y = 1000;
+	temp.y = 2000;
 	
 	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 6));
 	temp.x = 981 + 224;
-	temp.y = 900;
+	temp.y = 1900;
 
 	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 4));
 	temp.x = 1173 + 192;
-	temp.y = 804;
+	temp.y = 1804;
 
 	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 7));
 	temp.x = 688 + 224;
-	temp.y = 718;
+	temp.y = 1718;
 
 	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 5));
 	temp.x = 664;
-	temp.y = 604;
+	temp.y = 1504;
+
+	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 7));
+	temp.x = 470;
+	temp.y = 1408;
+
+	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 6));
+	temp.x = 16;
+	temp.y = 504;
+
+	temp = this.ground.add(new PlatformA(this.game, atlas, frame, 6));
+	temp.x = 408;
+	temp.y = 448;
 }
 
 World.prototype.loadWalls = function(atlas, frame) {
 	let temp = this.walls.add(new WallA(this.game, atlas, frame, 3));
-	temp.x = 1141 + 224;
-	temp.y = 804;
+	temp.x = 1365;
+	temp.y = 1804;
 
-	temp = this.walls.add(new WallA(this.game, atlas, frame, 5));
-	temp.x = 588 + 224;
-	temp.y = 604;
+	temp = this.walls.add(new WallA(this.game, atlas, frame, 7));
+	temp.x = 812;
+	temp.y = 1504;
 
-	temp = this.walls.add(new WallA(this.game, atlas, frame, 5));
-	temp.x = 588 + 64 + 12;
-	temp.y = 604 - (5*32);
+	temp = this.walls.add(new WallA(this.game, atlas, frame, 3));
+	temp.x = 664;
+	temp.y = 1408;
+
+	temp = this.walls.add(new WallA(this.game, atlas, frame, 20));
+	temp.x = 280;
+	temp.y = 704;
+
+	temp = this.walls.add(new WallA(this.game, atlas, frame, 20));
+	temp.x = 408;
+	temp.y = 576;
 }
 
 World.prototype.loadEnemies = function() {
